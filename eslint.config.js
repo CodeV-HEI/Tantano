3eee6952b3604289a46d225d1039ce594bd734ca
RaJharit77 <hei.rajo.2@gmail.com>
2026-02-10 15:39:55 +0300

    feat: Update login page, register page, dashboard page, eslint config and add three.js dependency with Background component

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,48 @@ module.exports = defineConfig([
   {
     ignores: ['dist/*'],
   },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'react/no-unknown-property': [
+        'error',
+        {
+          ignore: [
+            // Three.js properties
+            'position',
+            'args',
+            'transparent',
+            'opacity',
+            'color',
+            'attach',
+            'intensity',
+            'wireframe',
+            'rotation',
+            'scale',
+            'geometry',
+            'material',
+            'castShadow',
+            'receiveShadow',
+            // Other Three.js specific props
+            'frustumCulled',
+            'matrixAutoUpdate',
+            'quaternion',
+            'visible',
+            'uuid',
+            'layers',
+            'up',
+            'type',
+            'id',
+            'name',
+            'parent',
+            'modelViewMatrix',
+            'normalMatrix',
+            'matrixWorld',
+            'matrixWorldNeedsUpdate',
+            'children',
+          ],
+        },
+      ],
+    },
+  },
 ]);
